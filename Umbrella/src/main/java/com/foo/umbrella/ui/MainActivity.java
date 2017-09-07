422f52caa056b3919f02120c8491d0c7ce71e461
ingHectorVera <verahector26@gmail.com>
2017-09-07 15:21:31 -0400

    Intent service in the manifest

--- Umbrella/src/main/java/com/foo/umbrella/ui/MainActivity.java
+++ Umbrella/src/main/java/com/foo/umbrella/ui/MainActivity.java
@@ -67,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("zipCode",zipCode);
         startService(intent);
 
-
         /*ApiServicesProvider api = new ApiServicesProvider(getApplication());
 
         Call<WeatherData> call = api.getWeatherService().forecastForZipCallable(zipCode);
