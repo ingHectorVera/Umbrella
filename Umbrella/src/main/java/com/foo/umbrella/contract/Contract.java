4b9e0eee4be4c8d1ac7f485ddcd45432ce9cb39e
Hector Vera <verahector26@gmail.com>
2018-02-11 19:40:11 -0600

    Add Contract and Model

---
+++ Umbrella/src/main/java/com/foo/umbrella/contract/Contract.java
@@ -0,0 +1,8 @@
+package com.foo.umbrella.contract;
+
+/**
+ * Created by Hector Vera on 2/11/2018.
+ */
+
+public interface Contract {
+}
---
+++ Umbrella/src/main/java/com/foo/umbrella/model/ForecastModel.java
@@ -0,0 +1,8 @@
+package com.foo.umbrella.model;
+
+/**
+ * Created by Hector Vera on 2/11/2018.
+ */
+
+public class ForecastModel {
+}
